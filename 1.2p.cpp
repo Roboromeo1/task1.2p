d41c93963b49bedf4c30aa6a6604c63a8e6838cb
Adhitya Ram <Adhityaram417@gmail.com>
2021-09-06 02:41:57 +1000

    Using Interrupts

--- 1.2p.cpp
+++ 1.2p.cpp
@@ -1,25 +1,23 @@
-int pirSensor {};
-void setup ()
-{
-  pinMode (13, OUTPUT);
-  pinMode (2, INPUT);
+bool on=false;
+void setup(){
+  pinMode(10,OUTPUT);
+  attachInterrupt(0,ledchange,FALLING);
   Serial.begin(9600);
+  
 }
 
-void loop ()
-{
-  pirSensor = digitalRead (2);
-  if (pirSensor == HIGH)
-    {
-      Serial.println('Alert Intruder Detected'); 
-      digitalWrite(13, HIGH);
-      
-    }
-  else
-    {
-    digitalWrite (13, LOW);
-        Serial.println('You are Safe Now');
-    }
-     
-  delay (10);
+void loop(){
+delay(5000000);
+}
+
+void ledchange(){
+on = !on;
+  if(!on){
+   Serial.println('You are Safe Now');
+  digitalWrite(10,LOW);
+  }else{
+     Serial.println('Alert Intruder Detected'); 
+  digitalWrite(10,HIGH);
+ 
+  }
 }
